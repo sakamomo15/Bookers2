2051c5a8240af058581215d39205a9f78f3984ef
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-17 06:41:20 +0000

    [Add] 4-work3 header

--- db/schema.rb
+++ db/schema.rb
@@ -43,17 +43,18 @@ ActiveRecord::Schema.define(version: 2024_02_17_043630) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "opinion"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
