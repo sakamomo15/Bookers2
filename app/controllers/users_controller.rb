1cd7530bd9eff060d5573481565cdb4b34f5ce7e
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-20 08:47:15 +0000

    [Add] 4-work3 #update#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(params[:id])
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
