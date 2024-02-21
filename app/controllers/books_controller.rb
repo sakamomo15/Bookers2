9f8908cdf99992129d6abc6000a915546d6a550a
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-21 07:52:19 +0000

    [Add] 4-work3 is_matching

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
-
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
   def create
     @user = current_user
     @book = Book.new
     @books = Book.all
-    
+
     @book_data = Book.new(book_params)
     @book_data.user_id = current_user.id #bookのtableに必要なuser_idは入力してもらわないので、ここで定義
-    
+
     if @book_data.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book_data.id)
@@ -37,7 +37,7 @@ class BooksController < ApplicationController
   def update
     @user = User.find(current_user.id)
     @book = Book.new
-    
+
     @book_data = Book.find(params[:id])
     if @book_data.update(book_params)
       flash[:notice] = "You have updated book successfully."
@@ -57,5 +57,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :opinion)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    user = User.find(book.user_id)
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 end
